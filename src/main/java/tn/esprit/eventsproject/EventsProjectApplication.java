563e8327c1667e7751c94f979595443f2d5e13d5
oussemajebali <oussema.jbali@esprit.tn>
2024-07-16 23:21:58 +0100

    commit to test automatic build in jenkins

--- src/main/java/tn/esprit/eventsproject/EventsProjectApplication.java
+++ src/main/java/tn/esprit/eventsproject/EventsProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class EventsProjectApplication {
+    // commit to test automatic build in jenkins
 
     public static void main(String[] args) {
         SpringApplication.run(EventsProjectApplication.class, args);
